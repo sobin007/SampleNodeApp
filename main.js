56bd503bceadc3d7ee9dc6bfaa387c7450b8de20
Sobin <sobinmathew988@gmail.com>
2017-10-03 16:03:35 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+http.createServer(function (request, response){
+
+response.writeHead(200, {'Content-Type':'text'});
+
+response.end('Hello World\n');
+}).listen(3000);
+
+console.log('server running at http://127.0.0.1:3000/');
